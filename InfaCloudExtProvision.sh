043143100ebbd77cd58727af473c95a7522a844e
gsvivek <gsvivek@gmail.com>
2015-11-21 06:53:32 +0530

    Update InfaCloudExtProvision.sh

--- InfaCloudExtProvision.sh
+++ InfaCloudExtProvision.sh
@@ -1,1 +1,2 @@
-/root/infaagent/jre/bin/java -jar /root/infaagent/CloudExtProvisionClient.jar "cloudUsername" $1 "cloudPassword" $2 "connectorUUIDs" $3 "identifier" $4
+uuid=$(sudo dmidecode | grep UUID)
+/root/infaagent/jre/bin/java -jar /root/infaagent/CloudExtProvisionClient.jar "cloudUsername" "$1" "cloudPassword" "$2" "connectorUUIDs" "$3" "identifier" "$uuid"
