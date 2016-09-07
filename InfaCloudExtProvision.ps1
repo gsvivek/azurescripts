b7fa8ade7306229d66fb6aaf290d052cdda90eec
gsvivek <gsvivek@gmail.com>
2016-09-07 16:05:09 +0530

    Update InfaCloudExtProvision.ps1

--- InfaCloudExtProvision.ps1
+++ InfaCloudExtProvision.ps1
@@ -1,11 +1,11 @@
-param([string]$cloudUsername, [string]$cloudPassword,[string]$connectorUUIDs,[string]$identifier)
+param([string]$cloudEmail, [string]$cloudPassword,[string]$connectorUUIDs)
 $pinfo = New-Object System.Diagnostics.ProcessStartInfo
 $pinfo.FileName = "C:\Program Files\Informatica Cloud Secure Agent\jre\bin\java.exe"
 $pinfo.RedirectStandardError = $true
 $pinfo.RedirectStandardOutput = $true
 $pinfo.UseShellExecute = $false
 $computerSystemProduct = Get-WmiObject -class Win32_ComputerSystemProduct -namespace root\CIMV2
-$pinfo.Arguments = '-jar C:\Program Files\Informatica Cloud Secure Agent\InfaCloudExtProvision.jar "cloudUsername" "$cloudUsername" "cloudPassword" "$cloudPassword" "connectorUUIDs" "$connectorUUIDs" "identifier" "$computerSystemProduct.UUID"'
+$pinfo.Arguments = '-jar C:\Program Files\Informatica Cloud Secure Agent\InfaCloudExtProvision.jar "cloudEmail" "$cloudEmail" "cloudPassword" "$cloudPassword" "connectorUUIDs" "$connectorUUIDs" "identifier" "$computerSystemProduct.UUID"'
 $p = New-Object System.Diagnostics.Process
 $p.StartInfo = $pinfo
 $p.Start() | Out-Null
