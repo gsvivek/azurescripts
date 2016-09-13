3a122e10c117d32451b09318f96652b6eb76f416
gsvivek <gsvivek@gmail.com>
2016-09-14 00:39:15 +0530

    Add files via upload

--- InfaCloudExtProvision.ps1
+++ InfaCloudExtProvision.ps1
@@ -1,5 +1,9 @@
-param([string]$cloudEmail, [string]$cloudPassword,[string]$cloudName, [string]$cloudTitle, [string]$cloudPhone, [string]$cloudOrgName, [string]$cloudCountry, [string]$connectorUUIDs)
+param([string]$cloudEmail, [string]$cloudPassword, [string]$connectorUUIDs)
 $computerSystemProduct = Get-WmiObject -class Win32_ComputerSystemProduct -namespace root\CIMV2
-$result = & "C:\Program Files\Informatica Cloud Secure Agent\jre\bin\java.exe" -jar "C:\Program Files\Informatica Cloud Secure Agent\InfaCloudExtProvision.jar" "cloudEmail" "$cloudEmail" "cloudPassword" "$cloudPassword" "firstName" "$cloudName" "title" "$cloudTitle" "phone" "$cloudPhone" "orgName" "$cloudOrgName" "orgCountry" "$cloudCountry" "connectorUUIDs" "$connectorUUIDs" "identifier" $computerSystemProduct.UUID | ConvertFrom-Json
+$result = & "C:\Program Files\Informatica Cloud Secure Agent\jre\bin\java.exe" -jar "C:\Program Files\Informatica Cloud Secure Agent\InfaCloudExtProvision.jar" "cloudEmail" "$cloudEmail" "cloudPassword" "$cloudPassword" "connectorUUIDs" "$connectorUUIDs" "identifier" $computerSystemProduct.UUID | ConvertFrom-Json
 $status = $result.response.status
 if(-Not $status.CompareTo("success")) {Stop-Computer -force}
+else {
+& "C:\Program Files\Informatica Cloud Secure Agent\main\agentcore\consoleAgentManager.bat" configure "$cloudEmail" "$cloudPassword"
+& "C:\Program Files\Informatica Cloud Secure Agent\agent_start.bat"
+}
