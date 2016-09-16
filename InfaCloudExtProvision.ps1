634f27f105e6ea22effaacc4551a9ed391d16ab6
gsvivek <gsvivek@gmail.com>
2016-09-16 11:09:42 +0530

    Update InfaCloudExtProvision.ps1

--- InfaCloudExtProvision.ps1
+++ InfaCloudExtProvision.ps1
@@ -1,10 +1,13 @@
 param([string]$cloudEmail, [string]$cloudPassword, [string]$connectorUUIDs)
 $computerSystemProduct = Get-WmiObject -class Win32_ComputerSystemProduct -namespace root\CIMV2
 $result = & "C:\Program Files\Informatica Cloud Secure Agent\jre\bin\java.exe" -jar "C:\Program Files\Informatica Cloud Secure Agent\InfaCloudExtProvision.jar" "cloudEmail" "$cloudEmail" "cloudPassword" "$cloudPassword" "connectorUUIDs" "$connectorUUIDs" "identifier" $computerSystemProduct.UUID | ConvertFrom-Json
-$status = $result.response.status
-if(-Not $status.CompareTo("success")) {Stop-Computer -force}
-else {
+$msg = $result.response.msg
+if($msg.CompareTo("success")) {
 Remove-Item "C:\Program Files\Informatica Cloud Secure Agent\InfaCloudExtProvision.jar"
 & "C:\Program Files\Informatica Cloud Secure Agent\main\agentcore\consoleAgentManager.bat" configure "$cloudEmail" "$cloudPassword"
 & "C:\Program Files\Informatica Cloud Secure Agent\agent_start.bat"
+}
+else {
+Remove-Item "C:\Program Files\Informatica Cloud Secure Agent\InfaCloudExtProvision.jar"
+Stop-Computer -force
 }
