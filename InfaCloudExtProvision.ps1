5780507369530f42886f6213213c200f1c8dfab7
gsvivek <gsvivek@gmail.com>
2016-09-22 22:03:49 +0530

    Add files via upload

--- InfaCloudExtProvision.ps1
+++ InfaCloudExtProvision.ps1
@@ -2,14 +2,19 @@ param([string]$cloudUsername, [string]$cloudEmail, [string]$cloudPassword, [stri
 $computerSystemProduct = Get-WmiObject -class Win32_ComputerSystemProduct -namespace root\CIMV2
 $result = & "C:\Program Files\Informatica Cloud Secure Agent\jre\bin\java.exe" -jar "C:\Program Files\Informatica Cloud Secure Agent\InfaCloudExtProvision.jar" "cloudUsername" "$cloudUsername" "cloudEmail" "$cloudEmail" "cloudPassword" "$cloudPassword" "firstName" "$firstName" "lastName" "$lastName" "title" "$title" "orgName" "$orgName" "phone" "$phone" "connectorUUIDs" "$connectorUUIDs" "identifier" $computerSystemProduct.UUID "orgAddress" $computerSystemProduct.UUID | ConvertFrom-Json
 $status = $result.response.status
-Write-Host "API Status: " $status
-Write-Host "API Msg: " $result.response.msg
+$msg = $result.response.msg
 if($status -eq "success") {
+Write-Host "Registration successful on Informatica Cloud. Please login to the VM to use your Informatica Cloud subscription."
 Remove-Item "C:\Program Files\Informatica Cloud Secure Agent\InfaCloudExtProvision.jar"
 Start-Process "C:\Program Files\Informatica Cloud Secure Agent\apps\agentcore\agent_start.bat"
 Start-Process "C:\Program Files\Informatica Cloud Secure Agent\apps\agentcore\consoleAgentManager.bat" "configure $cloudUsername $cloudPassword"
 }
 else {
+Write-Error "Error occured while registering user on Informatica Cloud. VM has been shutdown but not deallocated and you will incur charges. Please stop the VM from the Azure portal to stop incurring charges."
+if($msg -eq "UserAlreadyExists") {Write-Error "Error Details: User already exists with the username " $cloudUsername}
+ElseIf ($msg -eq "ConnectorActivationFailed") {Write-Error "Error Details: Connector activation failed."}
+Else {Write-Error "An unexpected error has occurred. Please try again later or contact support."}
 Remove-Item "C:\Program Files\Informatica Cloud Secure Agent\InfaCloudExtProvision.jar"
 Stop-Computer -force
+exit 1905
 }
--- InfaCloudExtProvision.sh
+++ InfaCloudExtProvision.sh
@@ -2,15 +2,25 @@ UUID="$(sudo dmidecode | grep 'UUID' | sed -e 's/.*: //')"
 result="$(/usr/local/informatica-cloud/infaagent/jre/bin/java -jar /usr/local/informatica-cloud/infaagent/InfaCloudExtProvision.jar "cloudUsername" "$1" "cloudEmail" "$2" "cloudPassword" "$3" "firstName" "$4" "lastName" "$5" "title" "$6" "orgName" "$7" "phone" "$8" "connectorUUIDs" "$9" "identifier" "${UUID}" "orgAddress" "${UUID}")"
 status=$(echo "${result}" | jq '.response.status' | sed "s/\"//g")
 msg=$(echo "${result}" | jq '.response.msg'| sed "s/\"//g")
-echo "Status: ${status}"
-echo "Msg: ${msg}"
 if [[ ${status} = "success" ]]
 then
+	echo "Registration successful on Informatica Cloud. Please login to the VM to use your Informatica Cloud subscription."
 	rm "/usr/local/informatica-cloud/infaagent/InfaCloudExtProvision.jar"
 	cd /usr/local/informatica-cloud/infaagent/apps/agentcore
 	./infaagent startup &&
 	./consoleAgentManager.sh configure "$1" "$3"
 else
+	echo "Error occured while registering user on Informatica Cloud. VM has been shutdown but not deallocated and you will incur charges. Please stop the VM from the Azure portal to stop incurring charges."
+	if [[ ${msg} = "UserAlreadyExists" ]] 
+	then
+		echo "Error Details: User already exists with the username $1"
+	elif [[ ${msg} = "ConnectorActivationFailed" ]] 
+	then
+		echo "Error Details: Connector activation failed."
+	else
+		Else {Write-Error "An unexpected error has occurred. Please try again later or contact support."}
+	fi
 	rm "/usr/local/informatica-cloud/infaagent/InfaCloudExtProvision.jar"
 	shutdown -P now
+	exit 1905
 fi
