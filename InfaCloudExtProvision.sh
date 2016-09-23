2ccae152b67b94c5b82fae438fde6e937411bfd9
gsvivek <gsvivek@gmail.com>
2016-09-23 16:16:17 +0530

    Add files via upload

--- InfaCloudExtProvision.sh
+++ InfaCloudExtProvision.sh
@@ -1,12 +1,13 @@
+sudo su
 UUID="$(sudo dmidecode | grep 'UUID' | sed -e 's/.*: //')"
-result="$(/usr/local/informatica-cloud/infaagent/jre/bin/java -jar /usr/local/informatica-cloud/infaagent/InfaCloudExtProvision.jar "cloudUsername" "$1" "cloudEmail" "$2" "cloudPassword" "$3" "firstName" "$4" "lastName" "$5" "title" "$6" "orgName" "$7" "phone" "$8" "connectorUUIDs" "$9" "identifier" "${UUID}" "orgAddress" "${UUID}")"
+result="$(/usr/local/infaagent/jre/bin/java -jar /usr/local/infaagent/InfaCloudExtProvision.jar "cloudUsername" "$1" "cloudEmail" "$2" "cloudPassword" "$3" "firstName" "$4" "lastName" "$5" "title" "$6" "orgName" "$7" "phone" "$8" "connectorUUIDs" "$9" "identifier" "${UUID}" "orgAddress" "${UUID}")"
 status=$(echo "${result}" | jq '.response.status' | sed "s/\"//g")
 msg=$(echo "${result}" | jq '.response.msg'| sed "s/\"//g")
 if [[ ${status} = "success" ]]
 then
 	echo "Registration successful on Informatica Cloud. Please login to the VM to use your Informatica Cloud subscription."
-	rm "/usr/local/informatica-cloud/infaagent/InfaCloudExtProvision.jar"
-	cd /usr/local/informatica-cloud/infaagent/apps/agentcore
+	rm "/usr/local/infaagent/InfaCloudExtProvision.jar"
+	cd /usr/local/infaagent/apps/agentcore
 	./infaagent startup &&
 	./consoleAgentManager.sh configure "$1" "$3"
 else
@@ -20,7 +21,7 @@ else
 	else
 		Else {Write-Error "An unexpected error has occurred. Please try again later or contact support."}
 	fi
-	rm "/usr/local/informatica-cloud/infaagent/InfaCloudExtProvision.jar"
+	rm "/usr/local/infaagent/InfaCloudExtProvision.jar"
 	shutdown -P now
 	exit 1905
 fi
